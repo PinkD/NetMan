9e21fbb27f1280e57d658b12537fb3c305a69cf5
PinkD <443657547@qq.com>
2017-08-01 17:33:33 +0800

    update about activity

--- app/src/main/java/moe/pinkd/netman/ui/activity/AboutActivity.java
+++ app/src/main/java/moe/pinkd/netman/ui/activity/AboutActivity.java
@@ -1,6 +1,11 @@
 package moe.pinkd.netman.ui.activity;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -9,6 +14,8 @@ import moe.pinkd.netman.R;
 
 public class AboutActivity extends Activity implements View.OnClickListener, View.OnLongClickListener {
 
+    private Dialog dialog;
+    private ClipboardManager clipboardManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,28 +24,36 @@ public class AboutActivity extends Activity implements View.OnClickListener, Vie
         findViewById(R.id.pay_pinkd_now).setOnClickListener(this);
         findViewById(R.id.about_developer).setOnClickListener(this);
         findViewById(R.id.about_app).setOnClickListener(this);
-        findViewById(R.id.open_source_license).setOnClickListener(this);
         findViewById(R.id.about_version_code).setOnLongClickListener(this);
-
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.pay_pinkd_now:
-                Toast.makeText(AboutActivity.this, R.string.pay_pinkd_now, Toast.LENGTH_SHORT).show();
+                if (dialog == null) {
+                    dialog = new Dialog(this);
+                    dialog.setTitle(R.string.donation);
+                    dialog.setContentView(R.layout.pay_pinkd_now);
+                    dialog.findViewById(R.id.address_btc).setOnClickListener(this);
+                    dialog.findViewById(R.id.address_ali_pay).setOnClickListener(this);
+                    clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                }
+                dialog.show();
                 break;
-
             case R.id.about_developer:
-                Toast.makeText(AboutActivity.this, R.string.about_developer, Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/PinkD")));
                 break;
-
             case R.id.about_app:
-                Toast.makeText(AboutActivity.this, R.string.about_this_app, Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/PinkD/NetMan")));
                 break;
-
-            case R.id.open_source_license:
-                Toast.makeText(AboutActivity.this, R.string.about_license, Toast.LENGTH_SHORT).show();
+            case R.id.address_btc:
+                clipboardManager.setPrimaryClip(ClipData.newPlainText(getText(R.string.btc), getText(R.string.btc_address)));
+                Toast.makeText(this, R.string.copied, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.address_ali_pay:
+                clipboardManager.setPrimaryClip(ClipData.newPlainText(getText(R.string.ali_pay), getText(R.string.ali_pay_address)));
+                Toast.makeText(this, R.string.copied, Toast.LENGTH_SHORT).show();
                 break;
         }
     }
@@ -47,7 +62,7 @@ public class AboutActivity extends Activity implements View.OnClickListener, Vie
     public boolean onLongClick(View v) {
         switch (v.getId()) {
             case R.id.about_version_code:
-                Toast.makeText(AboutActivity.this, R.string.version_code, Toast.LENGTH_SHORT).show();
+                Toast.makeText(AboutActivity.this, R.string.pay_pinkd_now, Toast.LENGTH_SHORT).show();
                 break;
         }
         return false;
